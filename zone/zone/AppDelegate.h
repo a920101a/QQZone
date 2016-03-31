64274d0d01414fdfcdc87262c3f054d8ea69e809
xxxx--- <xxxx--->
2016-03-31 16:51:24 +0800

    初始化框架

---
+++ zone/zone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zone
+//
+//  Created by cyun on 16/3/31.
+//  Copyright © 2016年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zone/zone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zone
+//
+//  Created by cyun on 16/3/31.
+//  Copyright © 2016年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
